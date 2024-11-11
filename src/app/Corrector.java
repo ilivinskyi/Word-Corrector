e67ca8f9a1169e9de07481fb6e4b7e39e699c70e
Ihor L <gamesandservices@protonmail.com>
2024-11-11 18:53:07 +0100

    fix word-corrector code

---
+++ src/app/Corrector.java
@@ -0,0 +1,17 @@
+package app;
+
+public class Corrector {
+
+    public String handleData(String[] strs) {
+        var stringBuilder = new StringBuilder();
+        int count = 0;
+        for (String str : strs) {
+            if (str.contains("b")) {
+                str = str.replace("b", "o");
+            }
+            count++;
+            stringBuilder.append(count).append(") ").append(str).append("\n");
+        }
+        return stringBuilder.toString();
+    }
+}
